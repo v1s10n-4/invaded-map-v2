890bc757820dfa8b881fab8ecf559b6af7eb6c74
v1s10n_4 <romain.daniel@epitech.eu>
2025-10-20 04:06:16 +0200

    fix: env var usage

--- lib/auth.ts
+++ lib/auth.ts
@@ -38,7 +38,6 @@ export const auth = betterAuth({
     },
   },
   secret: process.env.BETTER_AUTH_SECRET ?? "",
-  baseURL: process.env.BETTER_AUTH_URL ?? "http://localhost:3000",
   plugins: [nextCookies()],
 });
 
